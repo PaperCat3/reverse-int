493e397bb9fe714c4957b0a15b1e867a7f388350
PaperCat3 <papercat03@gmail.com>
2022-06-22 14:24:22 +0700

    feat: code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+   let ab = Math.abs(n)
+   let numberString = String(ab) 
+   let numberReverse = ''
+   for (let i=numberString.length-1; i>=0; i--) {
+      numberReverse += numberString[i]
+   }
+   return numberReverse
 }
